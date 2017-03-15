afbe9e14f8f421d4dc1e9c310bc6977f5a319a7d
codexpage <ywx@MacBook-Pro-4.local>
2017-03-15 18:04:13 +0800

    fix the icon problem

--- hello.py
+++ hello.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.script import Manager
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+manger=Manager(app)
 
 @app.route("/")
 def hello():
 	return render_template("base.html")
 
 if __name__=="__main__":
-	app.run()
+	manger.run()
 	
