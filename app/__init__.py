4745ef6bdedcb2dc0daff370d578ef1444136177
codexpage <yywwxx1@gmail.com>
2017-03-17 14:19:30 +0800

    add password verification and login/logout function

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,11 @@ from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from config import config
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 #这些类必须写在函数外面，这样包内模块才能import
 bootstrap = Bootstrap()
@@ -23,7 +28,10 @@ def create_app(config_name): #app工厂函数
 	mail.init_app(app)
 	moment.init_app(app)
 	db.init_app(app)
+	login_manager.init_app(app)
 	#路由和自定义错误页面
 	from .main import main as main_blueprint #导入子包中的蓝本
 	app.register_blueprint(main_blueprint) #注册蓝本
+	from .auth import auth as auth_blueprint
+	app.register_blueprint(auth_blueprint, url_prefix='/auth') #这个蓝本中的路由都是/auth/xxx格式的
 	return app
--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
 #-*-encoding:utf-8-*-
 import os
 from app import create_app, db
