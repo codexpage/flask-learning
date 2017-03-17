9b506c8139a0184144aed6e6120f0a9861fc289b
codexpage <yywwxx1@gmail.com>
2017-03-17 20:06:20 +0800

    add change-email function

--- app/models.py
+++ app/models.py
@@ -76,5 +76,26 @@ class User(UserMixin, db.Model):
 		db.session.add(self)
 		return True
 
+	def generate_email_change_token(self, new_email, expiration=3600):
+		s = Serializer(current_app.config['SECRET_KEY'], expiration)
+		return s.dumps({'change_email': self.id, 'new_email': new_email})
+
+	def change_email(self, token):
+		s = Serializer(current_app.config['SECRET_KEY'])
+		try:
+			data = s.loads(token)
+		except:
+			return False
+		if data.get('change_email') != self.id:
+			return False
+		new_email = data.get('new_email')
+		if new_email is None: #新邮箱可能是空值，但是表单会做邮箱格式检查，所以这里不会为空，不需要检查
+			return False
+		if self.query.filter_by(email=new_email).first() is not None: #新邮箱已存在，表单已经做过检查了，所以这里其实不用再检查
+			return False
+		self.email = new_email
+		db.session.add(self)
+		return True	
+
 	def __repr__(self):
 		return '<User %r>' % self.username
