0aa8001956a80b64b4a5c50be1b4c57ea55334cb
codexpage <yywwxx1@gmail.com>
2017-03-22 16:09:40 +0800

    self-follower

--- app/models.py
+++ app/models.py
@@ -112,6 +112,7 @@ class User(UserMixin, db.Model):
 				self.role = Role.query.filter_by(permissions = 0xff).first()
 			if self.role is None:
 				self.role = Role.query.filter_by(default=True).first()
+		self.followed.append(Follow(followed=self))
 
 
 	@property
@@ -226,6 +227,14 @@ class User(UserMixin, db.Model):
 	@property
 	def followed_posts(self):
 		return Post.query.join(Follow,Follow.followed_id==Post.author_id,).filter(Follow.follower_id==self.id)
+
+	@staticmethod
+	def add_self_follows():
+		for user in User.query.all():
+			if not user.is_following(user):
+				user.follow(user)
+			db.session.add(user)
+			db.session.commit()
 
 
 	def __repr__(self):
